21074dc0ed3dc43b7b03bd50748c55ae056afd82
mhayescs19 <mhayescs19@gmail.com>
2024-10-17 22:48:21 -0700

    add mobile footer

--- project/src/app/(mobile)/dev/page.tsx
+++ project/src/app/(mobile)/dev/page.tsx
@@ -1,10 +1,19 @@
 import Question from "@/features/question/components/question";
-
+import Footer from "@/features/footer/components/footer";
 
 export default function Dev() {
-	return (
-		<div className="p-4">
-            <Question question="How often do you feel sad"></Question>   
-        </div>
-	);
+  return (
+    <div>
+      <div className="text-lg text-secondary font-bold pb-8">
+        Please answer the questions
+      </div>
+      <div className="pb-6">
+        Over the last two weeks, how often have you been bothered by any of the
+        following problems?
+      </div>
+      <Question question="How often do you feel sad"></Question>
+
+      <Footer />
+    </div>
+  );
 }
--- project/src/features/question/components/question.tsx
+++ project/src/features/question/components/question.tsx
@@ -4,37 +4,48 @@ import { useState } from "react";
 import { ChevronRight } from "lucide-react";
 import { QuestionInfo } from "../types/types";
 
-export default function Question({
-	question
-}: QuestionInfo) {
-	const [selected, setSelected] = useState<number | null>(null);
+export default function Question({ question }: QuestionInfo) {
+  const [selected, setSelected] = useState<number | null>(null);
 
-	const options = [0, 1, 2, 3];
+  const options = [0, 1, 2, 3];
 
-    const scale = ["Not at all", "Several days", "More than half the days", "Nearly every day"];
+  const scale = [
+    "Not at all",
+    "Several days",
+    "More than half the days",
+    "Nearly every day",
+  ];
 
-	return (
-		<div> 
-			<div className="text-secondary font-bold pb-2">
-				{question}
-			</div>
-			<div className="grid grid-cols-4 gap-1">
-				{options.map((option, index) => (
-					<div className="flex flex-col items-center">
-						<button
-							key={option}
-							className={`flex gap-2 h-16 w-full px-4 justify-between items-center ${selected === option ? 'bg-primary' : 'bg-secondaryLight'} ${index === 0 ? 'rounded-l-2xl' : index === options.length - 1 ? 'rounded-r-2xl' : 'rounded-none'}`}
-							onClick={() => setSelected(option)}
-						>
-						</button>
-						<span className={`text-center text-sm font-medium mt-2 ${selected === option ? 'text-primary' : 'text-secondary'}`}>{scale[index]}</span>
-					</div>
-				))}
-			</div>
-            <div>
-                {selected}
-            </div>
-		</div>
-		
-	);
+  return (
+    <div className="p-4">
+      <div className="text-secondary font-bold pb-2">{question}</div>
+      <div className="grid grid-cols-4 gap-1">
+        {options.map((option, index) => (
+          <div className="flex flex-col items-center">
+            <button
+              key={option}
+              className={`flex gap-2 h-16 w-full px-4 justify-between items-center ${
+                selected === option ? "bg-primary" : "bg-secondaryLight"
+              } ${
+                index === 0
+                  ? "rounded-l-2xl"
+                  : index === options.length - 1
+                  ? "rounded-r-2xl"
+                  : "rounded-none"
+              }`}
+              onClick={() => setSelected(option)}
+            ></button>
+            <span
+              className={`text-center text-sm font-medium mt-2 ${
+                selected === option ? "text-primary" : "text-secondary"
+              }`}
+            >
+              {scale[index]}
+            </span>
+          </div>
+        ))}
+      </div>
+      <div>{selected}</div>
+    </div>
+  );
 }
