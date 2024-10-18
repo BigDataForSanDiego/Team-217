bd534228d5be135582cf3099fef7f9a30707a357
jaxonhill <jaxoncharleshill@gmail.com>
2024-10-18 12:40:03 -0700

    Add PHQ questions to mental health form

--- project/src/app/(mobile)/mental-health/page.tsx
+++ project/src/app/(mobile)/mental-health/page.tsx
@@ -1,9 +1,10 @@
 import Question from "@/features/question/components/question";
+import { PHQ9 } from "@/features/question/utils/utils";
 import { ChevronLeft } from "lucide-react";
 
 export default function MentalHealth() {
 	return (
-		<div className="pt-16 px-4">
+		<div className="pt-16 px-4 pb-8">
 			<div className="flex flex-col gap-6">
 				<div className="flex items-center gap-2">
 					<ChevronLeft className="h-5 w-5 stroke-primary" />
@@ -17,10 +18,14 @@ export default function MentalHealth() {
 				Over the last two weeks, how often have you been bothered by any
 				of the following problems?
 			</h2>
-			<div className="pt-12 flex flex-col gap-6">
-				<Question question="How often do you feel sad" />
-				<Question question="Another test question" />
+			<div className="pt-12 flex flex-col gap-8">
+				{PHQ9.map((item) => (
+					<Question key={item.question} question={item.question} />
+				))}
 			</div>
+			<button className="mt-12 w-full py-2 rounded-full bg-primary text-white font-medium">
+				Next
+			</button>
 		</div>
 	);
 }
