3fc1c93ff6dca9b120c5073e9ba47b0e4d5413b6
jaxonhill <jaxoncharleshill@gmail.com>
2024-10-18 12:33:48 -0700

    Add layout for question page

--- project/src/app/(mobile)/mental-health/page.tsx
+++ project/src/app/(mobile)/mental-health/page.tsx
@@ -1,3 +1,26 @@
+import Question from "@/features/question/components/question";
+import { ChevronLeft } from "lucide-react";
+
 export default function MentalHealth() {
-	return <div>Mental Health</div>;
+	return (
+		<div className="pt-16 px-4">
+			<div className="flex flex-col gap-6">
+				<div className="flex items-center gap-2">
+					<ChevronLeft className="h-5 w-5 stroke-primary" />
+					<span className="text-primary font-medium">Back</span>
+				</div>
+				<h1 className="font-bold text-2xl text-secondary">
+					Please answer the questions
+				</h1>
+			</div>
+			<h2 className="pt-12 font-medium text-secondary">
+				Over the last two weeks, how often have you been bothered by any
+				of the following problems?
+			</h2>
+			<div className="pt-12 flex flex-col gap-6">
+				<Question question="How often do you feel sad" />
+				<Question question="Another test question" />
+			</div>
+		</div>
+	);
 }
